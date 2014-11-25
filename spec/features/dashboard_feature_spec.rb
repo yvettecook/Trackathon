99212f6453f03d0bec07b4f2c54508537f4eea09
Victoria Barr <vb329@georgetown.edu>
2014-11-25 18:50:44 +0000

    Tests all passing; hardcoded in September

--- spec/features/dashboard_feature_spec.rb
+++ spec/features/dashboard_feature_spec.rb
@@ -9,13 +9,14 @@ describe 'On the dashboard page' do
     it 'should have welcome widget' do
       Hackathon.create(name: :september, id: 1 )
       visit '/hackathons/1'
+      save_and_open_page
       expect(page).to have_css('#welcome-widget')
     end
 
     it 'should have a title of hello' do
       Hackathon.create(name: :september, id: 1 )
       visit '/hackathons/1'
-      expect(page).to have_css('#welcome-widget[data-title="Hello"]')
+      expect(page).to have_css('#welcome-widget[data-title="Hello September"]')
     end
 
   end
