61eab618178fa14aa3ced4f9e614711da1fa53bd
yvettecook <yvettecook@gmail.com>
2014-11-25 19:57:01 +0000

    Added a clock. untested. shhhhhh

--- app/controllers/hackathons_controller.rb
+++ app/controllers/hackathons_controller.rb
@@ -2,7 +2,5 @@ class HackathonsController < ApplicationController
 
   def show
     @hackathon = Hackathon.find(params[:id])
-    @name = @hackathon.name
-    redirect_to '/dashing/dashboards/september'
   end
 end
