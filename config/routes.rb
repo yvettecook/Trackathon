44446893c904215c3513db30d6ad3af7251d3a2a
yvettecook <yvettecook@gmail.com>
2014-11-25 15:55:03 +0000

    Created first resources and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :hackathon, shallow: true do
+    resources :projects
+  end
+
+  root to: "home#index"
+
+end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +63,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
