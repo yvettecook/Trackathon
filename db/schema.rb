f5aaf74e28dcce4a77be2b0f27a05389b770c932
yvettecook <yvettecook@gmail.com>
2014-11-26 10:08:55 +0000

    Test for clock widget passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20141125163233) do
-
+ActiveRecord::Schema.define(version: 20141125165248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "hackathons", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "projects", force: true do |t|
     t.string   "name"
--- spec/features/dashboard_feature_spec.rb
+++ spec/features/dashboard_feature_spec.rb
@@ -2,22 +2,27 @@ require 'rails_helper'
 
 describe 'On the dashboard page' do
 
-
-
   context 'welcome widget' do
 
-    it 'should have welcome widget' do
+    before do
       Hackathon.create(name: :september, id: 1 )
+    end
+
+    it 'should have welcome widget' do
       visit '/hackathons/1'
       expect(page).to have_css('#welcome-widget')
     end
 
     it 'should have a title of hello' do
-      Hackathon.create(name: :september, id: 1 )
       visit '/hackathons/1'
-      save_and_open_page
       expect(page).to have_css('#welcome-widget[data-title="Hello September"]')
     end
+
+    it 'should have a clock widget' do
+      visit 'hackathons/1'
+      expect(page).to have_css('#clock-widget')
+    end
+
 
   end
 
