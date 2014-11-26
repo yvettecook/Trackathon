db6a44b861f80dcac841fa7bc33e04b0b221983b
yvettecook <yvettecook@gmail.com>
2014-11-26 10:35:01 +0000

    Countdown widget has time remaining

--- spec/features/dashboard_feature_spec.rb
+++ spec/features/dashboard_feature_spec.rb
@@ -1,28 +1,36 @@
 require 'rails_helper'
 
 describe 'On the dashboard page' do
+    before do
+      Hackathon.create(name: :september, id: 1 )
+      visit '/hackathons/1'
+    end
 
   context 'welcome widget' do
 
-    before do
-      Hackathon.create(name: :september, id: 1 )
-    end
-
     it 'should have welcome widget' do
-      visit '/hackathons/1'
       expect(page).to have_css('#welcome-widget')
     end
 
     it 'should have a title of hello' do
-      visit '/hackathons/1'
       expect(page).to have_css('#welcome-widget[data-title="Hello September"]')
     end
 
-    it 'should have a clock widget' do
-      visit 'hackathons/1'
-      expect(page).to have_css('#clock-widget')
+  end
+
+  context 'countdown timer widget' do
+
+    it 'should have a countdown widget' do
+      expect(page).to have_css('#countdown-widget')
     end
 
+    it 'should have a title of time remaining' do
+      expect(page).to have_css('#countdown-widget[data-title="Time remaining"]')
+    end
+
+    it 'should have the finish time of the hackathon' do
+      expect(page).to have_css('#countdown-widget[data-finishtime="05-Dec-2014 17:00:00"]')
+    end
 
   end
 
