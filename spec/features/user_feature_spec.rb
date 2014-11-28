d4b0931fc9fe25cab254bd1e55e36da77b25cca1
EllaNancyFay <ellaschofield0@gmail.com>
2014-11-28 16:02:34 +0000

    Link from homepage to create new hackathon

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe 'The user journey' do
 
-  context 'on the homepage' do
+  context 'on the homepage the user' do
 
-    it 'the user should see Welcome to the Hackathon Tracker' do
+    it 'should see Welcome to the Hackathon Tracker' do
       visit '/'
       expect(page).to have_content 'Welcome to the Hackathon Tracker'
+    end
+
+    it 'should see link to create a new Hackathon' do
+      visit '/'
+      expect(page).to have_link 'New Hackathon'
     end
 
   end
