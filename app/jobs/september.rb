6e3eb8b43c87ac0a0426da10da58c59b7b399264
alexfakhri <alexfakhri@me.com>
2014-11-28 12:32:06 +0000

    Currently choosing between dashing and projects jquery.

--- app/jobs/september.rb
+++ app/jobs/september.rb
@@ -3,9 +3,17 @@ Dashing.scheduler.every '1s' do
   Dashing.send_event('project', { value: project_value })
 end
 
+
 def get_project_status
 	project = Project.last
-	product_status = project.product
-	return 0 if product_status.nil? || product_status == false
-	return 20 if product_status == true
+	
+	project_status = 0
+	
+	milestones = [project.product, project.design, project.frontdev, project.backdev, project.presentation]
+	
+	milestones.each do |milestone|
+		project_status += 20 if milestone == true
+	end
+
+	return project_status
 end
