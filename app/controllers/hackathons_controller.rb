01be9e9f90e40174fca3cac843df2b7685bddd19
Sam Joseph <tansaku@gmail.com>
2014-11-28 19:11:05 +0000

    other notes

--- app/controllers/hackathons_controller.rb
+++ app/controllers/hackathons_controller.rb
@@ -12,7 +12,7 @@ class HackathonsController < ApplicationController
 
   def create
   	@hackathon = Hackathon.create(params.require(:hackathon).permit(:name, :end_time))
-  	redirect_to '/dashing/dashboards/september' # hard coded
+  	redirect_to '/dashing/dashboards/september' # hard coded --> '/dashing/dashboards/project'
   end
 
 
