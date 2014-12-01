5b5d9468c1fb8dc122453dc6b794d74ec7d07688
Victoria Barr <vb329@georgetown.edu>
2014-12-01 11:57:29 +0000

    Scheduler finds all projects and updates

--- app/jobs/september.rb
+++ app/jobs/september.rb
@@ -19,15 +19,24 @@
 # end
 
 Dashing.scheduler.every '1s' do
-	project_value1 = get_project_status(1)
-	project_value2 = get_project_status(2)
-	Dashing.send_event('project1', { value: project_value1 })
-	Dashing.send_event('project2', { value: project_value2 })
+	
+	Project.all.each do |project| 
+		id = project.id
+		project_value = get_project_status(id)
+		Dashing.send_event("project#{id}", { value: project_value })
+	end
+
+	# project_value2 = get_project_status(2)
+	# Dashing.send_event('project2', { value: project_value2 })
 end
 
 def get_project_status(id)
 
-	project = Project.find_by_id(id)
+	project = Project.find_by(id: id)
+	puts "***************"
+	puts project
+	puts project.product
+	puts "***************"
 
 	project_status = 0
 
---
+++ spec/features/widgets/update_project_widgets_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'updating project widgets' do
+
+	let!(:september){Hackathon.create(name: "september", end_time: "2014-12-05 17:00:00 UTC")}
+	let!(:trackathon){september.projects.create(name: "Trackathon")}
+
+	before do
+		trackathon.update(product: true)
+	end
+
+	context "project progress widget", js: true do
+
+		it 'displays current project completion percentage' do
+			visit "/hackathons/#{september.id}"
+			expect(page.find("#project-progress-widget1 .project").value).to eq '20'
+		end
+	end
+end
