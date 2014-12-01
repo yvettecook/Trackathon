434155cd9cfe4b6aad9773698781d57ac608afd4
Victoria Barr <vb329@georgetown.edu>
2014-12-01 12:09:12 +0000

    scheduler tidied up

--- app/jobs/september.rb
+++ app/jobs/september.rb
@@ -1,23 +1,3 @@
-# Dashing.scheduler.every '1s' do
-# 	project_value = get_project_status
-#   Dashing.send_event('project1', { value: project_value })
-# end
-#
-#
-# def get_project_status
-# 	project = Project.last
-#
-# 	project_status = 0
-#
-# 	milestones = [project.product, project.design, project.frontdev, project.backdev, project.presentation]
-#
-# 	milestones.each do |milestone|
-# 		project_status += 20 if milestone == true
-# 	end
-#
-# 	return project_status
-# end
-
 Dashing.scheduler.every '1s' do
 	
 	Project.all.each do |project| 
@@ -26,17 +6,11 @@ Dashing.scheduler.every '1s' do
 		Dashing.send_event("project#{id}", { value: project_value })
 	end
 
-	# project_value2 = get_project_status(2)
-	# Dashing.send_event('project2', { value: project_value2 })
 end
 
 def get_project_status(id)
 
 	project = Project.find_by(id: id)
-	puts "***************"
-	puts project
-	puts project.product
-	puts "***************"
 
 	project_status = 0
 
--- spec/features/widgets/update_project_widgets_spec.rb
+++ spec/features/widgets/update_project_widgets_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe 'updating project widgets' do
 
 	let!(:september){Hackathon.create(name: "september", end_time: "2014-12-05 17:00:00 UTC")}
-	let!(:trackathon){september.projects.create(name: "Trackathon")}
+	let!(:trackathon){september.projects.create(name: "Trackathon", product: true)}
 
-	before do
-		trackathon.update(product: true)
-	end
+	# before do
+	# 	trackathon.update(product: true)
+	# end
 
 	context "project progress widget", js: true do
 
