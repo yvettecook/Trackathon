6a7614f4fb3b5e39be986d0e6d6fb2e8937fd64c
Victoria Barr <vb329@georgetown.edu>
2014-12-02 15:15:24 +0000

    Rewrote the omniauth method; signs in most people via github, just doesn't seem to like Alex for some reason

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.find_for_github_oauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      @user.save
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "github") if is_navigational_format?
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,31 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
 
-  def self.from_omniauth(auth)
-	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.email = auth.info.email
-	    user.password = Devise.friendly_token[0,20]
-	  end
-	end
-end
+ #  def self.from_omniauth(auth)
+	#   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	#     user.email = auth.info.email
+	#     user.password = Devise.friendly_token[0,20]
+	#   end
+	# end
+
+	def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    user = signed_in_resource || User.where(:provider => auth.provider, :uid => auth.uid).first
+     unless user
+		   user = User.create(
+			   provider:auth.provider,
+			   uid:auth.uid,
+			   email:auth.info.email,
+			   password:Devise.friendly_token[0,20]
+		 )
+			end
+			user
+		end
+  end
+
+	def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
