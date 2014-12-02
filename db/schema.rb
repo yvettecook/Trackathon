38749500e2eebf4573cb750d01dae13c4ae4d49a
alexfakhri <alexfakhri@me.com>
2014-12-02 18:22:07 +0000

    Styling the nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -50,11 +50,8 @@ ActiveRecord::Schema.define(version: 20141202135030) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-<<<<<<< HEAD
     t.string   "provider"
     t.string   "uid"
-=======
->>>>>>> d2cdd3c5ed54e675072d816aab48c4b1a6b44241
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
