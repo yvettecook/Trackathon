d41ae524ab72946338132646850bc1c008ac4905
EllaNancyFay <ellaschofield0@gmail.com>
2014-12-02 15:42:25 +0000

    Added description field to new project form

--- spec/features/project_start_feature_spec.rb
+++ spec/features/project_start_feature_spec.rb
@@ -13,20 +13,36 @@ describe "New project page" do
 
   it 'has a new project form' do
     expect(page).to have_field 'project_name'
+    expect(page).to have_field 'project_description'
   end
 
-  it 'creates a new project' do
+end
+
+describe "Filling in form" do
+
+  before do
+    @september = Hackathon.create(name: 'September', end_time: "2014-12-05 17:00:00 UTC")
+    visit "/hackathons/#{@september.id}/projects/new"
     fill_in :project_name, with: 'Robot Army'
-    save_and_open_page
-    click_button 'create'
-    save_and_open_page
+    fill_in :project_description, with: 'Snozzas barmy army'
+    click_button 'Create'
+  end
+
+
+  it 'creates a new project' do
     expect(Project.all.count).to eq 1 
   end
 
   it "should redirect to the project view page when a project's created" do
-    fill_in :project_name, with: 'Robot Army'
-    click_button 'Create'
     expect(current_path).to eq '/projects/2'
+  end
+
+  it 'should show the project name' do
+    expect(page).to have_content 'Robot Army'
+  end
+
+  it 'should show the project description' do
+    expect(page).to have_content 'Snozzas barmy army'
   end
 
 end
