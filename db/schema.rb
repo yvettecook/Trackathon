655e20c594d946208ee87bcbddeb04445ddd49e9
yvettecook <yvettecook@gmail.com>
2014-12-02 18:22:45 +0000

    Database updated

--- db/schema.rb
+++ db/schema.rb
@@ -39,22 +39,4 @@ ActiveRecord::Schema.define(version: 20141202161411) do
 
   add_index "projects", ["hackathon_id"], name: "index_projects_on_hackathon_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
