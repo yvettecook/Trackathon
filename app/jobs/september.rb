f42a78375500d57755e7ad1489df3470a5f0f277
yvettecook <yvettecook@gmail.com>
2014-12-02 19:44:11 +0000

    Tweets from Makers Academy showing on dashboard widget

--- app/jobs/september.rb
+++ app/jobs/september.rb
@@ -22,3 +22,36 @@ def get_project_status(id)
 
 	return project_status
 end
+
+
+
+require 'twitter'
+#### Get your twitter keys & secrets:
+#### https://dev.twitter.com/docs/auth/tokens-devtwittercom
+twitter = Twitter::REST::Client.new do |config|
+	config.consumer_key = 'W7KEItDGKLjM6zWoNOPdIYdPP'
+	config.consumer_secret = 'UrUFLMkigqw6U1pz3hYC9MMW4OGjOwwtsCNmjzTpbxz618Ex9s'
+	config.access_token = '122639798-4Nga2ptCMZmruyY3c6WNnxM4HrCQGJDezlAsiZvJ'
+	config.access_token_secret = '0nYttdbRztAuTuTKwz38OOd0j7039Jw6kgArEp2Mbm2CI'
+end
+
+search_term = URI::encode('@makersacademy')
+
+# Dashing.scheduler.every '5s', :first_in => 0 do |job|
+Dashing.scheduler.every '20s' do
+
+	begin
+		tweets = twitter.search("#{search_term}")
+
+		if tweets
+			tweets = tweets.map do |tweet|
+				{ name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
+			end
+			Dashing.send_event('twitter_mentions', comments: tweets)
+		end
+
+	rescue Twitter::Error
+		puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
+	end
+
+end
