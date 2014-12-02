22b071e1ff61c77304440f456f8b463cd743f92f
EllaNancyFay <ellaschofield0@gmail.com>
2014-12-02 10:50:40 +0000

    Added test runner script

---
+++ testrunner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "**************"
+echo "Tests starting"
+echo "**************"
+
+rspec spec/features/hackathon_start_feature_spec.rb
+rspec spec/features/multiple_projects_feature_spec.rb
+rspec spec/features/project_start_feature_spec.rb
+rspec spec/features/projects_milestones_feature_spec.rb
+rspec spec/features/user_feature_spec.rb
+rspec spec/features/widgets/new_project_widget_spec.rb
+rspec spec/features/widgets/project_progress_widget_spec.rb
+rspec spec/features/widgets/timer_widget_spec.rb
+rspec spec/features/widgets/update_project_widgets_spec.rb
+rspec spec/features/widgets/welcome_widget_spec.rb
+
+echo "**************"
+echo "Tests finished"
+echo "**************"
