fcfe28f68708ac2bd889674698c222742ca8835e
yvettecook <yvettecook@gmail.com>
2014-12-03 20:24:59 +0000

    List added, but github limits exceeded

--- app/jobs/list.rb
+++ app/jobs/list.rb
@@ -1,9 +1,33 @@
-buzzwords = ['Paradigm shift', 'Leverage', 'Pivoting', 'Turn-key', 'Streamlininess', 'Exit strategy', 'Synergy', 'Enterprise', 'Web 2.0']
-buzzword_counts = Hash.new({ value: 0 })
+require 'net/http'
+require 'json'
 
-Dashing.scheduler.every '2s' do
-  random_buzzword = buzzwords.sample
-  buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
+repos = [{owner: "yvettecook", name: "trackathon"}, {owner: "jamesascarter", name: "stack-overtube-frontend"}, {owner: "MadameSardine", name: "pandapop"}, {owner: "Schlap", name: "project-noderover"}, {owner: "Scully87", name: "FATS_badgr"}]
 
-  Dashing.send_event('gitlines', { items: buzzword_counts.values })
+repo_info = Hash.new{}
+
+  Dashing.scheduler.every '5s' do
+
+    repo = repos.sample
+      response = Net::HTTP.get(URI("https://api.github.com/repos/#{repo[:owner]}/#{repo[:name]}/stats/code_frequency?client_id=2e1dfe8b2f7b1c3608a6?client_secret=09f3b23d695d1ae79a174adb37d1e21a548f772b"))
+      data = JSON.parse(response)
+
+      number = 0
+
+      data.each do |week|
+        puts week[1]
+        puts week[2]
+        # number += week[1]
+        # number += week[2]
+      end
+
+      name = repo[:name]
+
+      repo_info[name] = { label: name, value: number }
+
+
+
+    Dashing.send_event('gitlines', { items: repo_info.values })
+    # buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
+
+
 end
--- app/jobs/september.rb
+++ app/jobs/september.rb
@@ -1,24 +1,24 @@
-Dashing.scheduler.every '2s' do
-
-	Project.all.each do |project|
-		id = project.id
-		project_value = get_project_status(id)
-		Dashing.send_event("project#{id}", { value: project_value })
-	end
-
-end
-
-def get_project_status(id)
-
-	project = Project.find_by(id: id)
-
-	project_status = 0
-
-	milestones = [project.product, project.design, project.frontdev, project.backdev, project.presentation]
-
-	milestones.each do |milestone|
-		project_status += 20 if milestone == true
-	end
-
-	return project_status
-end
+# Dashing.scheduler.every '2s' do
+#
+# 	Project.all.each do |project|
+# 		id = project.id
+# 		project_value = get_project_status(id)
+# 		Dashing.send_event("project#{id}", { value: project_value })
+# 	end
+#
+# end
+#
+# def get_project_status(id)
+#
+# 	project = Project.find_by(id: id)
+#
+# 	project_status = 0
+#
+# 	milestones = [project.product, project.design, project.frontdev, project.backdev, project.presentation]
+#
+# 	milestones.each do |milestone|
+# 		project_status += 20 if milestone == true
+# 	end
+#
+# 	return project_status
+# end
