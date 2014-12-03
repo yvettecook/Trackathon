ba3e477eb656ec249618375ccef275d1e8b92e30
yvettecook <yvettecook@gmail.com>
2014-12-03 09:11:43 +0000

    Changed twitter frequency

--- app/jobs/twitter.rb
+++ app/jobs/twitter.rb
@@ -1,6 +1,5 @@
 require 'twitter'
 
-
 twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = Rails.application.secrets.twitter_consumer_key
   config.consumer_secret = Rails.application.secrets.twitter_consumer_secret
