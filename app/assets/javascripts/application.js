2031480d6231301e02cfa038d5754bfb460cea2f
Camilla van Klinken <camillavk@gmail.com>
2014-12-03 14:02:39 +0000

    trying staging environment without twitter widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,27 +14,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require projects
-
-console.log("Hells yeah! The dashboard has started!");
-
-Dashing.on('ready', function() {
-  var contentWidth;
-  Dashing.widget_margins || (Dashing.widget_margins = [5, 5]);
-  Dashing.widget_base_dimensions || (Dashing.widget_base_dimensions = [300, 360]);
-  Dashing.numColumns || (Dashing.numColumns = 5);
-  contentWidth = (Dashing.widget_base_dimensions[0] + Dashing.widget_margins[0] * 2) * Dashing.numColumns;
-  return Batman.setImmediate(function() {
-    $('.gridster').width(contentWidth);
-    return $('.gridster ul:first').gridster({
-      widget_margins: Dashing.widget_margins,
-      widget_base_dimensions: Dashing.widget_base_dimensions,
-      avoid_overlapped_widgets: !Dashing.customGridsterLayout,
-      draggable: {
-        stop: Dashing.showGridsterInstructions,
-        start: function() {
-          return Dashing.currentWidgetPositions = Dashing.getWidgetPositions();
-        }
-      }
-    });
-  });
-});
