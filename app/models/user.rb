4950c34b707bf4a1b62e4ad52d097eda543ca0cd
Victoria Barr <vb329@georgetown.edu>
2014-12-03 10:45:17 +0000

    User sign up & sign in via Github merged into development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
 
- #  def self.from_omniauth(auth)
-	#   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	#     user.email = auth.info.email
-	#     user.password = Devise.friendly_token[0,20]
-	#   end
-	# end
-
 	def self.find_for_github_oauth(auth, signed_in_resource=nil)
-		puts "**************"
-		puts auth.info.email
-		puts "**************"
-
     user = signed_in_resource || User.where(:provider => auth.provider, :uid => auth.uid).first
      	unless user
      		if auth.info.email
