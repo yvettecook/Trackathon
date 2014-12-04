88f4a0d6d63063ee84e4473d1330b1fd6ab3d1f5
Camilla van Klinken <camillavk@gmail.com>
2014-12-04 10:59:28 +0000

    removed reaping frequency

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -1,10 +1,11 @@
 Rails.application.config.after_initialize do
-  # ActiveRecord::Base.connection_pool.disconnect!
-  #
-  # ActiveSupport.on_load(:active_record) do
-  #   config = Rails.application.config.database_configuration[Rails.env]
-  #   config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-  #   config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 20
-  #   ActiveRecord::Base.establish_connection(config)
-  # end
+  ActiveRecord::Base.connection_pool.disconnect!
+
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+          Rails.application.config.database_configuration[Rails.env]
+    # config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 20
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
