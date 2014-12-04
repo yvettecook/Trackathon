a1fa58d96193d6e12d20c6e8f4b494e575b25321
Camilla van Klinken <camillavk@gmail.com>
2014-12-04 16:27:42 +0000

    moved twitter env variables to twitter.rb

--- app/jobs/twitter.rb
+++ app/jobs/twitter.rb
@@ -1,10 +1,21 @@
 require 'twitter'
 
-twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = Rails.application.secrets.twitter_consumer_key
-  config.consumer_secret = Rails.application.secrets.twitter_consumer_secret
-  config.access_token = Rails.application.secrets.twitter_access_token
-  config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+if Rails.env.development?
+  twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key = Rails.application.secrets.twitter_consumer_key
+    config.consumer_secret = Rails.application.secrets.twitter_consumer_secret
+    config.access_token = Rails.application.secrets.twitter_access_token
+    config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+  end
+end
+
+if Rails.env.production?
+  twitter = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
 end
 
 search_term = URI::encode('#septmakers')
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,6 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.secret_key_base = ENV['SECRET_KEY_BASE']
-  config.twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY']
-  config.twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-  config.twitter_access_token = ENV['TWITTER_ACCESS_TOKEN']
-  config.twitter_access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
