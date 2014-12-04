09a2ea10e2d740334c2f106cf7a25d6b4b8a2a7d
yvettecook <yvettecook@gmail.com>
2014-12-04 12:13:58 +0000

    More merges

--- app/jobs/github.rb
+++ app/jobs/github.rb
@@ -1,17 +1,32 @@
-# require 'net/http'
-# require 'json'
-#
-# repos = [{owner: "yvettecook", name: "trackathon"}, {owner: "jamesascarter", name: "stack-overtube-frontend"}, {owner: "MadameSardine", name: "pandapop"}, {owner: "Schlap", name: "project-noderover"}, {owner: "Scully87", name: "FATS_badgr"}, {owner: "karinnielsen", name: "Final-Project-POSTit"}]
-#
-# repos.each do |repo|
-#   response = Net::HTTP.get(URI("https://api.github.com/repos/#{repo[:owner]}/#{repo[:name]}/stats/code_frequency"))
-#   data = JSON.parse(response)
-#
-#   number = 0
-#   data.each do |week|
-#   	number += (week[1] + week[2])
-#   end
-#
-#   puts repo[:name] + ": total lines of code = " + number.to_s
-# end
-#
+require 'net/http'
+require 'json'
+
+repos = [{owner: "yvettecook", name: "trackathon"}, {owner: "MadameSardine", name: "pandapop"}, {owner: "Scully87", name: "FATS_badgr"}, {owner: "jamesascarter", name: "stack-overtube-frontend"}, {owner: "karinnielsen", name: "Final-Project-POSTit"}, {owner: "Schlap", name: "project-noderover"}]
+
+repo_info = Hash.new{}
+total_lines = 0
+
+Dashing.scheduler.every '60s' do
+
+  repos.each do |repo|
+
+    response = Net::HTTP.get(URI("https://api.github.com/repos/#{repo[:owner]}/#{repo[:name]}/stats/code_frequency?access_token=244b19..."))
+    data = JSON.parse(response)
+
+    number = 0
+
+    data.each do |week|
+      number += week[1]
+      number += week[2]
+    end
+
+    name = repo[:name]
+
+    repo_info[name] = { label: name, value: number }
+
+    Dashing.send_event('gitlines', { items: repo_info.values })
+
+  end
+
+
+end
