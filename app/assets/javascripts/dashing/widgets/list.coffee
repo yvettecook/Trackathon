d61ffd755f900e5c65ba904180ab0323ad81c5de
Victoria Barr <vb329@georgetown.edu>
2014-12-04 11:46:06 +0000

    Displaying lines of code for each project; fiddled with CSS

--- app/assets/javascripts/dashing/widgets/list.coffee
+++ app/assets/javascripts/dashing/widgets/list.coffee
@@ -1,6 +1,11 @@
 class Dashing.List extends Dashing.Widget
+	@accessor 'total', Dashing.AnimatedValue
+
   ready: ->
     if @get('unordered')
       $(@node).find('ol').remove()
     else
       $(@node).find('ul').remove()
+
+  
+
--- app/jobs/list.rb
+++ app/jobs/list.rb
@@ -1,33 +1,32 @@
 require 'net/http'
 require 'json'
 
-repos = [{owner: "yvettecook", name: "trackathon"}, {owner: "jamesascarter", name: "stack-overtube-frontend"}, {owner: "MadameSardine", name: "pandapop"}, {owner: "Schlap", name: "project-noderover"}, {owner: "Scully87", name: "FATS_badgr"}]
+repos = [{owner: "yvettecook", name: "trackathon"}, {owner: "MadameSardine", name: "pandapop"}, {owner: "Scully87", name: "FATS_badgr"}, {owner: "jamesascarter", name: "stack-overtube-frontend"}, {owner: "karinnielsen", name: "Final-Project-POSTit"}, {owner: "Schlap", name: "project-noderover"}]
 
 repo_info = Hash.new{}
+total_lines = 0
 
-  Dashing.scheduler.every '5s' do
+  Dashing.scheduler.every '60s' do
 
-    repo = repos.sample
-      response = Net::HTTP.get(URI("https://api.github.com/repos/#{repo[:owner]}/#{repo[:name]}/stats/code_frequency?client_id=2e1dfe8b2f7b1c3608a6?client_secret=09f3b23d695d1ae79a174adb37d1e21a548f772b"))
+    repos.each do |repo|
+      
+      response = Net::HTTP.get(URI("https://api.github.com/repos/#{repo[:owner]}/#{repo[:name]}/stats/code_frequency?access_token=244b19a62812ffda9640451deb1dbc87c14d618b"))
       data = JSON.parse(response)
 
       number = 0
 
       data.each do |week|
-        puts week[1]
-        puts week[2]
-        # number += week[1]
-        # number += week[2]
+        number += week[1]
+        number += week[2]
       end
 
       name = repo[:name]
 
       repo_info[name] = { label: name, value: number }
 
-
-
     Dashing.send_event('gitlines', { items: repo_info.values })
-    # buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
+
+  end
 
 
 end
