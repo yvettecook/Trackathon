6ee144fa7f4be8fab36c901f872aee5400264bdd
Yvette Cook <yvettecook@gmail.com>
2014-12-17 14:57:13 +0000

    Update project_progress_widget_spec.rb

--- spec/features/dashboard/widgets/project_progress_widget_spec.rb
+++ spec/features/dashboard/widgets/project_progress_widget_spec.rb
@@ -12,10 +12,6 @@ describe 'Project progress widget' do
       expect(page).to have_css('#project-progress-widget1')
     end
 
-    xit 'should display the completed project percentage', js: true do
-      expect(page).to have_css('#project-progress-widget1 input.project[data-bind-value="20"]')
-    end
-
     it 'should display the project name from the database', js: true do
       expect(page.find('#project-progress-widget2 h1')).to have_content('Trackathon')
     end
